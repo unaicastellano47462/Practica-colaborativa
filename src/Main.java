e162c4805c1c3aa91740acaac7bbaaabfdd833d6
Urtzi <urtzi.maruri.48440@ikasle.egibide.org>
2025-10-29 12:05:59 +0100

    Version 1 Urtzi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
-public class Main() {
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
 
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite su nombre: ");
+        String nome = input.nextLine();
     }
 }
